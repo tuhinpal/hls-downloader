815f212d0bb82460d44d54e220c52083d75dd254
Tuhin Kanti Pal <me@thetuhin.com>
2025-04-28 21:21:01 +0530

    refactor: remove unused createChunk function from chunker.ts

--- src/lib/chunker.ts
+++
@@ -1,9 +0,0 @@
-const createChunk = <T>(data: T[], chunkSize: number): T[][] => {
-  const chunks: T[][] = [];
-  for (let i = 0; i < data.length; i += chunkSize) {
-    chunks.push(data.slice(i, i + chunkSize));
-  }
-  return chunks;
-};
-
-export default createChunk;
